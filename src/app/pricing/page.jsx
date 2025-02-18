af31d255c968c15a822ee6477d950cfb876b99fd
KozetaPajaj <kozetapajaj27@gmail.com>
2025-02-18 12:39:30 -0500

    Improve project structure

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -1,11 +1,23 @@
-'use client';
+/**** 
+  IMPORTANT: This page is a Server Component by default. 
+  DO NOT add "use client"; unless necessary.
+  Keep this as a Server Component for better performance.
+
+  Best Practice:
+    - Keep **page.jsx** as a Server Component.
+    - Create a separate **Client Component** inside the same folder and import it.
+****/
+
+export const metadata = {
+  title: 'Olympic Fit - Pricing',
+}
 
 export default function Pricing() {
-    return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                This is the Pricing Page
-            </main>
-        </div>
-    )
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-mulish)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        This is the Pricing Page
+      </main>
+    </div>
+  );
 }
