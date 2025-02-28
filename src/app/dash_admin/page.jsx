7a0c95ea909dd678b6747476b0a0d3766bfcc910
EdwinEras <edwineras23@gmail.com>
2025-02-28 11:37:58 -0500

    trainer dashboard without functionality done

--- src/app/dash_admin/page.jsx
+++ src/app/dash_admin/page.jsx
@@ -14,12 +14,11 @@ import ManageClass from "./ManageClass";
 import ManageTrainner from "./ManageTrainer";
 import ManageMembers from "./ManageMembers";
 
-
 export default function DashAdmin() {
   const arrClasses = [
-    { id: 1, name: "Yoga 1", description: "this is the descoption for Yoga 1", active: "yes", start_time: "2025-05-01T00:00", end_time: "2025-08-01T00:00", category:"Regular", capacity:10, location:"In person", media:"http://example.com"},
-    { id: 2, name: "Zumba 1", description: "this is the descoption for Zumba 1", active: "no", start_time: "2025-05-01T00:00", end_time: "2025-08-01T00:00", category:"Intermedia", capacity:5, location:"Online", media:"http://example.com"},
-    { id: 3, name: "Cardio 1", description: "this is the descoption for Cardio 1", active: "yes", start_time: "2025-05-01T00:00", end_time: "2025-08-01T00:00", category:"Advance", capacity:15, location:"In person", media:"http://example.com"}
+    { id: 1, name: "Yoga 1", description: "this is the description for Yoga 1", active: "yes", start_time: "2025-05-01T00:00", end_time: "2025-08-01T00:00", category:"Regular", capacity:10, location:"In person", media:"http://example.com"},
+    { id: 2, name: "Zumba 1", description: "this is the description for Zumba 1", active: "no", start_time: "2025-05-01T00:00", end_time: "2025-08-01T00:00", category:"Intermedia", capacity:5, location:"Online", media:"http://example.com"},
+    { id: 3, name: "Cardio 1", description: "this is the description for Cardio 1", active: "yes", start_time: "2025-05-01T00:00", end_time: "2025-08-01T00:00", category:"Advance", capacity:15, location:"In person", media:"http://example.com"}
   ];
 
   const arrTrainers = [
