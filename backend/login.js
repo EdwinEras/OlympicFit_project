3c680488638ae30c1a5447f9817450126598ec1b
teddy-adim <nwabuisiteddy@yahoo.com>
2025-04-13 15:04:46 -0400

    fixing password hash problem

--- backend/login.js
+++ backend/login.js
@@ -1,20 +1,23 @@
 const { client } = require('./mongodbConnector');
+const bcrypt = require('bcrypt');
 require("dotenv").config();
 
 const loginUser = async (email, password) => {
-    const filter = { email:email, password_hash:password };
-    const db = client.db(process.env.DBNAME);
-    const collection = db.collection('users');
-    const user = await collection.findOne(filter)
-    .then((json)=>{
-        return json;
-    })
-    .catch(()=>{
-        console.log(err);
-        return { message: "DB ERROR" }
-    });
-    console.log(user);
-    return user;
-}
+  const db = client.db(process.env.DBNAME);
+  const collection = db.collection('users');
+
+  const user = await collection.findOne({ email });
+
+  if (!user) return { success: false, message: "User not found" };
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) return { success: false, message: "Invalid credentials" };
+
+  // Remove password before returning user
+  const { password: _, ...safeUser } = user;
+
+  return { success: true, user: safeUser };
+};
 
 module.exports = { loginUser };
--- backend/users.js
+++ backend/users.js
@@ -1,88 +1,90 @@
 const { ObjectId } = require('mongodb');
 const { client } = require('./mongodbConnector');
+const bcrypt = require('bcrypt');
 require("dotenv").config();
 
-const createUser = async ( data ) => {
+const createUser = async (data) => {
   data._id = new ObjectId();
+
+  // Generate a username if one doesn't exist
+  if (!data.username) {
+    data.username = `user_${Date.now()}`;
+  }
+
+  // Hash password before saving
+  if (data.password) {
+    const saltRounds = 10;
+    data.password = await bcrypt.hash(data.password, saltRounds);
+    console.log("✅ Password after hashing:", data.password);
+  }
+
   const db = client.db(process.env.DBNAME);
   const collection = db.collection('users');
-  const user = await collection.insertOne(data)
-  .then((json) => {
-    return json;
-  })
-  .catch((err) => {
-    console.log(err);
-    return { message: "DB ERROR" }
-  });
-  return user;
+
+  try {
+    const user = await collection.insertOne(data);
+    return user;
+  } catch (err) {
+    console.error("❌ DB Insert Error:", err);
+    return { message: "DB ERROR" };
+  }
 };
 
 const getUsers = async () => {
   const db = client.db(process.env.DBNAME);
   const collection = db.collection('users');
-  const users = await collection.find({}).toArray()
-  .then((json)=>{
-    return json;
-  })
-  .catch(()=>{
-    console.log(err);
-    return { message: "DB ERROR" }
-  });
-  return users;
-}
+  try {
+    const users = await collection.find({}).toArray();
+    return users.map(({ password, ...u }) => u); // Strip password
+  } catch (err) {
+    console.error(err);
+    return { message: "DB ERROR" };
+  }
+};
 
 const getUserById = async (id) => {
-  if (!ObjectId.isValid(id)) {
-    return { message: "Invalid request" }
-  }
-  const filter = { _id: new ObjectId(id) };
-  const db = client.db(process.env.DBNAME);
-  const collection = db.collection('users');
-  const user = await collection.findOne(filter)
-  .then((json)=>{
-    return json;
-  })
-  .catch(()=>{
-    console.log(err);
-    return { message: "DB ERROR" }
-  });
-  return user;
-}
+  if (!ObjectId.isValid(id)) return { message: "Invalid request" };
 
-const updateUser = async ( id, data ) => {
-  if (!ObjectId.isValid(id)) {
-    return { message: "Invalid request" }
-  }
-  const filter = { _id: new ObjectId(id) };
   const db = client.db(process.env.DBNAME);
   const collection = db.collection('users');
-  const users = await collection.updateOne(filter, { $set: data })
-  .then((json)=>{
-    return json;
-  })
-  .catch(()=>{
-    console.log(err);
-    return { message: "DB ERROR" }
-  });
-  return users;
-}
 
-const deleteUser = async ( id ) => {
-  if (!ObjectId.isValid(id)) {
-    return { message: "Invalid request" }
+  try {
+    const { password, ...user } = await collection.findOne({ _id: new ObjectId(id) });
+    return user;
+  } catch (err) {
+    console.error(err);
+    return { message: "DB ERROR" };
   }
-  const filter = { _id: new ObjectId(id) };
+};
+
+const updateUser = async (id, data) => {
+  if (!ObjectId.isValid(id)) return { message: "Invalid request" };
+
   const db = client.db(process.env.DBNAME);
   const collection = db.collection('users');
-  const user = await collection.deleteOne(filter)
-  .then((json) => {
-    return json;
-  })
-  .catch((err) => {
-    console.log(err);
-    return { message: "DB ERROR" }
-  });
-  return user;
+
+  try {
+    const user = await collection.updateOne({ _id: new ObjectId(id) }, { $set: data });
+    return user;
+  } catch (err) {
+    console.error(err);
+    return { message: "DB ERROR" };
+  }
+};
+
+const deleteUser = async (id) => {
+  if (!ObjectId.isValid(id)) return { message: "Invalid request" };
+
+  const db = client.db(process.env.DBNAME);
+  const collection = db.collection('users');
+
+  try {
+    const user = await collection.deleteOne({ _id: new ObjectId(id) });
+    return user;
+  } catch (err) {
+    console.error(err);
+    return { message: "DB ERROR" };
+  }
 };
 
 module.exports = { createUser, getUsers, getUserById, updateUser, deleteUser };
